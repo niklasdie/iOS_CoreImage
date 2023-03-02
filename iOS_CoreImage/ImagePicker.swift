b4b9620ce83bac61258ffbf8702b513454516777
Niklas Diekhoener <niklasdiekhoener@gmail.com>
2023-03-02 11:44:37 +0100

    init

---
+++ iOS_CoreImage/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  iOS_CoreImage
+//
+//  Created by Niklas Diekhöner on 19.02.23.
+//
+
+import Foundation
---
+++ iOS_CoreImage/ImageSaver.swift
@@ -0,0 +1,8 @@
+//
+//  ImageSaver.swift
+//  iOS_CoreImage
+//
+//  Created by Niklas Diekhöner on 19.02.23.
+//
+
+import Foundation
